1e351d8df11ae71c1f2e230123f122ea43d3869d
Reinaldo Trindade <rewifetri@gmail.com>
2020-09-18 16:45:58 -0300

    Serving static files on uploads folder

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,11 @@ import express from "express";
 //
 import routes from "./routes";
 import "./db";
+import { uploadsDir } from "@config/upload";
 
 const server = express();
 server.use(express.json());
+server.use("/files", express.static(uploadsDir));
 server.use(routes);
 
 server.listen(4000, () => {
