62edfac8d85e52a3d9e6a9de2de640c1ec2d1904
Reinaldo Trindade <rewifetri@gmail.com>
2020-10-20 16:25:08 -0300

    Comments on tsyringe container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,20 @@
 import { container } from "tsyringe";
 
+// tsyringe is a dependency injection library.
+// This container will control all injections in the app.
+// As the services needs to receive the repository in their contructors, using
+// this method i can replace this syntax:
+
+// const eventsRepository = new EventsRepository();
+// const createEvent = new CreateEvent(eventsRepository);
+
+// With this:
+
+// const createEvent = container.resolve(CreateEvent);
+
+// To achieve this i must also use the `injectable` and `inject` decorators on the
+// services definition
+
 import IEventsRepository from "@modules/events/repositories/IEventsRepository";
 import EventsRepository from "@modules/events/infra/typeorm/repositories/EventsRepository";
 
@@ -15,8 +30,3 @@ container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
 );
-
-// const eventsRepository = new EventsRepository();
-// const createEvent = new CreateEvent(eventsRepository);
-
-// container.resolve(CreateEvent);
